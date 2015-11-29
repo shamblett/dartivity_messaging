ae43da62f70fffba2ab84807a24e5f7519a0448e
Steve Hamblett <steve.hamblett@linux.com>
2015-11-29 11:40:03 +0000

    Initial test structure - running

--- test/lib/dartivity_messaging_test.dart
+++ test/lib/dartivity_messaging_test.dart
@@ -11,91 +11,297 @@ import 'dart:async';
 import 'dart:io';
 
 import 'package:dartivity_messaging/dartivity_messaging.dart';
+import 'package:test/test.dart';
+import 'package:json_object/json_object.dart' as json;
 
-Future main() async {
-  final int badExitCode = -1;
+void main() {
+  group("Message Tests", () {
+    test("Who Has  - invalid source", () {
+      try {
+        DartivityMessage message = new DartivityMessage.whoHas(null, "");
+      } catch (e) {
+        expect(e.runtimeType.toString(), 'DartivityMessagingException');
+        expect(
+            e.toString(),
+            DartivityMessagingException.HEADER +
+                DartivityMessagingException.INVALID_WHOHAS_MESSAGE);
+      }
+    });
 
-  // Instantiate a Dartivity client and initialise for
-  // messaging only
-  Dartivity dartivity = new Dartivity(Mode.messagingOnly);
+    test("Who Has  - invalid resource name", () {
+      try {
+        DartivityMessage message = new DartivityMessage.whoHas("", null);
+      } catch (e) {
+        expect(e.runtimeType.toString(), 'DartivityMessagingException');
+        expect(
+            e.toString(),
+            DartivityMessagingException.HEADER +
+                DartivityMessagingException.INVALID_WHOHAS_MESSAGE);
+      }
+    });
 
-  // Send before ready
-  DartivityMessage noSend = new DartivityMessage.iHave("", "", "", {}, "");
-  var result = dartivity.send(noSend);
-  if (result == null) {
-    print("Message not sent not ready - OK");
-  } else {
-    print("Oops message sent when not ready- ERROR");
-    exit(badExitCode);
-  }
+    test("Who Has  - invalid host", () {
+      try {
+        DartivityMessage message =
+        new DartivityMessage.whoHas(null, null, null);
+      } catch (e) {
+        expect(e.runtimeType.toString(), 'DartivityMessagingException');
+        expect(
+            e.toString(),
+            DartivityMessagingException.HEADER +
+                DartivityMessagingException.INVALID_WHOHAS_MESSAGE);
+      }
+    });
 
-  // Initialise
-  await dartivity.initialise(
-      DartivityCfg.MESS_CRED_PATH, DartivityCfg.MESS_PROJECT_ID);
+    test("Who Has  - toJSON", () {
+      DartivityMessage message = new DartivityMessage.whoHas(
+          DartivityMessage.ADDRESS_WEB_SERVER, "oic/res", "localhost");
+      expect(message.toJSON(),
+          '{"type":0,"source":"web-server","destination":"global","resourceName":"oic/res","resourceDetails":{},"host":"localhost","provider":"Unknown"}');
+    });
 
-  if (dartivity.initialised) {
-    print("Initialse Status is true - OK");
-  } else {
-    print("Oops Initialse Status is false - ERROR");
-    exit(badExitCode);
-  }
+    test("Who Has  - fromJSON", () {
+      DartivityMessage message = new DartivityMessage.fromJSON(
+          '{"type":0,"source":"web-server","destination":"global","resourceName":"oic/res","resourceDetails":{},"host":"localhost", "provider":"Unknown"}');
+      expect(message.host, "localhost");
+      expect(message.destination, DartivityMessage.ADDRESS_GLOBAL);
+      expect(message.source, DartivityMessage.ADDRESS_WEB_SERVER);
+      expect(message.resourceDetails, {});
+      expect(message.resourceName, "oic/res");
+      expect(message.provider, DartivityMessage.PROVIDER_UNKNOWN);
+    });
 
-  // Send a null message
-  var result1 = dartivity.send(null);
-  if (result1 == null) {
-    print("Null message not sent - OK");
-  } else {
-    print("Oops null message sent - ERROR");
-    exit(badExitCode);
-  }
+    test("Who Has  - fromJSONObject", () {
+      json.JsonObject job = new json.JsonObject.fromJsonString(
+          '{"type":0,"source":"web-server","destination":"global","resourceName":"oic/res","resourceDetails":{},"host":"localhost", "provider":"Unknown"}');
+      DartivityMessage message = new DartivityMessage.fromJSONObject(job);
+      expect(message.host, "localhost");
+      expect(message.destination, DartivityMessage.ADDRESS_GLOBAL);
+      expect(message.source, DartivityMessage.ADDRESS_WEB_SERVER);
+      expect(message.resourceDetails, {});
+      expect(message.resourceName, "oic/res");
+      expect(message.provider, DartivityMessage.PROVIDER_UNKNOWN);
+    });
 
-  // Send a couple of whoHas messages
-  DartivityMessage whoHas1 =
-  new DartivityMessage.whoHas(dartivity.id, '/core/light');
-  String whoHas1Json = whoHas1.toJSON();
-  print("Who Has 1 >> ${whoHas1Json}");
-  dartivity.send(whoHas1);
-  DartivityMessage whoHas2 =
-  new DartivityMessage.whoHas(dartivity.id, '/core/thermostat');
-  dartivity.send(whoHas2);
+    test("Who Has - toString", () {
+      DartivityMessage message = new DartivityMessage.whoHas(
+          DartivityMessage.ADDRESS_WEB_SERVER, "oic/res", "localhost");
+      expect(message.toString(),
+          "Type : Type.whoHas, Provider : Unknown, Host : localhost, Source : web-server, Destination : global, Resource Name : oic/res, Resource Details : {}");
+    });
 
-  // Followed by a I Have
-  Map<String, String> myDevice = {'iama': 'thermostat', 'url': 'here.com/ami'};
-  DartivityMessage iHave = new DartivityMessage.iHave(
-      dartivity.id, dartivity.id, '/core/therm/1', myDevice, "");
-  dartivity.send(iHave);
+    test("I Have  - invalid source", () {
+      try {
+        DartivityMessage message =
+        new DartivityMessage.iHave(null, "", "", {}, "", "");
+      } catch (e) {
+        expect(e.runtimeType.toString(), 'DartivityMessagingException');
+        expect(
+            e.toString(),
+            DartivityMessagingException.HEADER +
+                DartivityMessagingException.INVALID_IHAVE_MESSAGE);
+      }
+    });
 
-  // Followed by a I Have but not for us
-  Map<String, String> myDevice1 = {'iama': 'thermostat', 'url': 'here.com/ami'};
-  DartivityMessage iHave1 = new DartivityMessage.iHave(
-      dartivity.id, 'not us', '/core/therm/1', myDevice1, "");
-  dartivity.send(iHave1);
+    test("I Have  - invalid destination", () {
+      try {
+        DartivityMessage message =
+        new DartivityMessage.iHave("", null, "", {}, "", "");
+      } catch (e) {
+        expect(e.runtimeType.toString(), 'DartivityMessagingException');
+        expect(
+            e.toString(),
+            DartivityMessagingException.HEADER +
+                DartivityMessagingException.INVALID_IHAVE_MESSAGE);
+      }
+    });
 
-  // Listen for our messages until our timer pops
-  var subscription;
-  int messCount = 0;
+    test("I Have  - invalid resource name", () {
+      try {
+        DartivityMessage message =
+        new DartivityMessage.iHave("", "", null, {}, "", "");
+      } catch (e) {
+        expect(e.runtimeType.toString(), 'DartivityMessagingException');
+        expect(
+            e.toString(),
+            DartivityMessagingException.HEADER +
+                DartivityMessagingException.INVALID_IHAVE_MESSAGE);
+      }
+    });
 
-  void timerCallback() {
-    print("Closing the client");
-    if (messCount == 3) {
-      print("Message count is 3 - OK");
-    } else {
-      print("Message count is ${messCount} - ERROR, should be 3");
-      exit(badExitCode);
-    }
-    subscription.cancel();
-    try {
-      dartivity.close();
-    } catch (e) {}
-    // Good exit
-    exit(0);
-  }
-  Timer timer = new Timer(
-      new Duration(seconds: (DartivityCfg.MESS_PULL_TIME_INTERVAL * 7 + 2)),
-      timerCallback);
+    test("I Have  - invalid resource details", () {
+      try {
+        DartivityMessage message =
+        new DartivityMessage.iHave("", "", "", null, "", "");
+      } catch (e) {
+        expect(e.runtimeType.toString(), 'DartivityMessagingException');
+        expect(
+            e.toString(),
+            DartivityMessagingException.HEADER +
+                DartivityMessagingException.INVALID_IHAVE_MESSAGE);
+      }
+    });
 
-  subscription = dartivity.nextMessage.listen((DartivityMessage message) {
-    print("Message received ${message.toString()}");
-    messCount++;
+    test("I Have  - invalid host", () {
+      try {
+        DartivityMessage message =
+        new DartivityMessage.iHave("", "", "", {}, null, "");
+      } catch (e) {
+        expect(e.runtimeType.toString(), 'DartivityMessagingException');
+        expect(
+            e.toString(),
+            DartivityMessagingException.HEADER +
+                DartivityMessagingException.INVALID_IHAVE_MESSAGE);
+      }
+    });
+
+    test("I Have  - invalid provider", () {
+      try {
+        DartivityMessage message =
+        new DartivityMessage.iHave("", "", "", {}, "", null);
+      } catch (e) {
+        expect(e.runtimeType.toString(), 'DartivityMessagingException');
+        expect(
+            e.toString(),
+            DartivityMessagingException.HEADER +
+                DartivityMessagingException.INVALID_IHAVE_MESSAGE);
+      }
+    });
+
+    test("I Have  - toJSON", () {
+      DartivityMessage message = new DartivityMessage.iHave(
+          DartivityMessage.ADDRESS_WEB_SERVER,
+          DartivityMessage.ADDRESS_GLOBAL,
+          "oic/res",
+          {},
+          "localhost",
+          DartivityMessage.PROVIDER_IOTIVITY);
+      expect(message.toJSON(),
+          '{"type":1,"source":"web-server","destination":"global","resourceName":"oic/res","resourceDetails":{},"host":"localhost","provider":"Iotivity"}');
+    });
+
+    test("I Have  - fromJSON", () {
+      DartivityMessage message = new DartivityMessage.fromJSON(
+          '{"type":1,"source":"web-server","destination":"global","resourceName":"oic/res","resourceDetails":{},"host":"localhost","provider":"Iotivity"}');
+      expect(message.host, "localhost");
+      expect(message.destination, DartivityMessage.ADDRESS_GLOBAL);
+      expect(message.source, DartivityMessage.ADDRESS_WEB_SERVER);
+      expect(message.resourceDetails, {});
+      expect(message.resourceName, "oic/res");
+      expect(message.provider, DartivityMessage.PROVIDER_IOTIVITY);
+    });
+
+    test("I Have  - fromJSONObject", () {
+      json.JsonObject job = new json.JsonObject.fromJsonString(
+          '{"type":1,"source":"web-server","destination":"global","resourceName":"oic/res","resourceDetails":{},"host":"localhost", "provider":"Iotivity"}');
+      DartivityMessage message = new DartivityMessage.fromJSONObject(job);
+      expect(message.host, "localhost");
+      expect(message.destination, DartivityMessage.ADDRESS_GLOBAL);
+      expect(message.source, DartivityMessage.ADDRESS_WEB_SERVER);
+      expect(message.resourceDetails, {});
+      expect(message.resourceName, "oic/res");
+      expect(message.provider, DartivityMessage.PROVIDER_IOTIVITY);
+    });
+
+    test("I Have  - toString", () {
+      DartivityMessage message = new DartivityMessage.iHave(
+          DartivityMessage.ADDRESS_WEB_SERVER,
+          DartivityMessage.ADDRESS_GLOBAL,
+          "oic/res",
+          {},
+          "localhost",
+          DartivityMessage.PROVIDER_UNKNOWN);
+      expect(message.toString(),
+          "Type : Type.iHave, Provider : Unknown, Host : localhost, Source : web-server, Destination : global, Resource Name : oic/res, Resource Details : {}");
+    });
   });
 }
+
+//
+//Future main() async {
+//  final int badExitCode = -1;
+//
+//  // Instantiate a Dartivity client and initialise for
+//  // messaging only
+//  Dartivity dartivity = new Dartivity(Mode.messagingOnly);
+//
+//  // Send before ready
+//  DartivityMessage noSend = new DartivityMessage.iHave("", "", "", {}, "");
+//  var result = dartivity.send(noSend);
+//  if (result == null) {
+//    print("Message not sent not ready - OK");
+//  } else {
+//    print("Oops message sent when not ready- ERROR");
+//    exit(badExitCode);
+//  }
+//
+//  // Initialise
+//  await dartivity.initialise(
+//      DartivityCfg.MESS_CRED_PATH, DartivityCfg.MESS_PROJECT_ID);
+//
+//  if (dartivity.initialised) {
+//    print("Initialse Status is true - OK");
+//  } else {
+//    print("Oops Initialse Status is false - ERROR");
+//    exit(badExitCode);
+//  }
+//
+//  // Send a null message
+//  var result1 = dartivity.send(null);
+//  if (result1 == null) {
+//    print("Null message not sent - OK");
+//  } else {
+//    print("Oops null message sent - ERROR");
+//    exit(badExitCode);
+//  }
+//
+//  // Send a couple of whoHas messages
+//  DartivityMessage whoHas1 =
+//  new DartivityMessage.whoHas(dartivity.id, '/core/light');
+//  String whoHas1Json = whoHas1.toJSON();
+//  print("Who Has 1 >> ${whoHas1Json}");
+//  dartivity.send(whoHas1);
+//  DartivityMessage whoHas2 =
+//  new DartivityMessage.whoHas(dartivity.id, '/core/thermostat');
+//  dartivity.send(whoHas2);
+//
+//  // Followed by a I Have
+//  Map<String, String> myDevice = {'iama': 'thermostat', 'url': 'here.com/ami'};
+//  DartivityMessage iHave = new DartivityMessage.iHave(
+//      dartivity.id, dartivity.id, '/core/therm/1', myDevice, "");
+//  dartivity.send(iHave);
+//
+//  // Followed by a I Have but not for us
+//  Map<String, String> myDevice1 = {'iama': 'thermostat', 'url': 'here.com/ami'};
+//  DartivityMessage iHave1 = new DartivityMessage.iHave(
+//      dartivity.id, 'not us', '/core/therm/1', myDevice1, "");
+//  dartivity.send(iHave1);
+//
+//  // Listen for our messages until our timer pops
+//  var subscription;
+//  int messCount = 0;
+//
+//  void timerCallback() {
+//    print("Closing the client");
+//    if (messCount == 3) {
+//      print("Message count is 3 - OK");
+//    } else {
+//      print("Message count is ${messCount} - ERROR, should be 3");
+//      exit(badExitCode);
+//    }
+//    subscription.cancel();
+//    try {
+//      dartivity.close();
+//    } catch (e) {}
+//    // Good exit
+//    exit(0);
+//  }
+//  Timer timer = new Timer(
+//      new Duration(seconds: (DartivityCfg.MESS_PULL_TIME_INTERVAL * 7 + 2)),
+//      timerCallback);
+//
+//  subscription = dartivity.nextMessage.listen((DartivityMessage message) {
+//    print("Message received ${message.toString()}");
+//    messCount++;
+//  });
+//}
